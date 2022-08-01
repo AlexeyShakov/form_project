0635b0e37cd4cc605b4eab7382eac1a86ab2e572
AlexeyShakov <alexei_96@inbox.ru>
2022-08-02 00:00:54 +0300

    made validation for django forms

---
+++ feedback/forms.py
@@ -0,0 +1,13 @@
+# This file allows us to get rid of hand-made HTML forms
+from django import forms
+
+
+class FeedbackForm(forms.Form):
+    name = forms.CharField(max_length=7, min_length=2, error_messages={
+        "max_length": "Слишком много символов",
+        "min_length": "Слишком мало символов",
+        "required": "Укажите хотя бы один символ"
+    })
+    # last_name = forms.CharField()
+    # feedback = forms.CharField(widget=forms.Textarea(attrs={"rows": 2, "cols": 40}))
+    rating = forms.IntegerField(max_value=5, min_value=1)
--- feedback/views.py
+++ feedback/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import FeedbackForm
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        # Take name(the name of the text area, see feedback.html) parameter from the request
-        name = request.POST["name"]
-        if len(name) == 0:
-            return render(request, 'feedback/feedback.html', context={"got_error": True})
-        print(name)
-        return HttpResponseRedirect("/done")
-    return render(request, 'feedback/feedback.html', context={"got_error": False})
+        form = FeedbackForm(request.POST)
+        # So we are just checking whether the form is filled or not. If yes we make redirect, if not return the clean form
+        if form.is_valid():
+            # Getting the dictionary out of request parameters
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/done")
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={"form": form})
 
 
 
