201d2cfbdfa88e3d1c8b23c671d4b67eb3b24896
AlexeyShakov <alexei_96@inbox.ru>
2022-08-02 18:54:41 +0300

    changed the dispay of forms by adding .css file

--- feedback/forms.py
+++ feedback/forms.py
@@ -8,6 +8,6 @@ class FeedbackForm(forms.Form):
         "min_length": "Слишком мало символов",
         "required": "Укажите хотя бы один символ"
     })
-    # last_name = forms.CharField()
-    # feedback = forms.CharField(widget=forms.Textarea(attrs={"rows": 2, "cols": 40}))
+    last_name = forms.CharField()
+    feedback = forms.CharField(widget=forms.Textarea(attrs={"rows": 2, "cols": 40}))
     rating = forms.IntegerField(max_value=5, min_value=1)
