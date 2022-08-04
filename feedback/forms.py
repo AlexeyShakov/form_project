ae30bba1c485ca80522bec804d9ad5751e6c832d
AlexeyShakov <alexei_96@inbox.ru>
2022-08-04 20:07:33 +0300

    Re-created GalleryView but inherrited from CreateView not View

--- feedback/forms.py
+++ feedback/forms.py
@@ -14,7 +14,7 @@ from .models import Feedback
 #     rating = forms.IntegerField(max_value=5, min_value=1)
 
 
-# The way of creating a form based the corresponding model
+# The way of creating a form based on the corresponding model
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
--- gallery/views.py
+++ gallery/views.py
@@ -3,23 +3,11 @@ from django.views import View
 from .forms import GalleryUploadForm
 from django.http import HttpResponseRedirect
 from .models import Gallery
+from django.views.generic.edit import CreateView
 
 
-
-# Create your views here.
-class GalleryView(View):
-    def get(self, request):
-        form = GalleryUploadForm()
-        # the parameter 'context' serves for sending the data to .html file
-        return render(request, "gallery/load_file.html", context={'form': form})
-
-    def post(self, request):
-        # Filling the form with data
-        form = GalleryUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Saves a file
-            # Creating an instance and save it with the model
-            new_image = Gallery(image=request.FILES['image'])
-            new_image.save()
-            return HttpResponseRedirect("load_image")
-        return render(request, "gallery/load_file.html", context={'form': form})
+class GalleryView(CreateView):
+    model = Gallery
+    fields = "__all__"
+    template_name = "gallery/load_file.html"
+    success_url = "load_image"
