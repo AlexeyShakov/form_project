ad0d6a8192ff76c2329dd096930f6f43dfab24cd
AlexeyShakov <alexei_96@inbox.ru>
2022-08-04 14:16:38 +0300

    Implemented the work with forms with FormView class

--- feedback/views.py
+++ feedback/views.py
@@ -3,28 +3,44 @@ from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
 from .models import Feedback
 from django.views import View
-from django.views.generic import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
-# Let's make our logic with a class way not functional
-class FeedBackView(View):
-    # The names of methods must be the same like HTTP methods
-    def get(self, request):
-        # Just create a blank form
-        form = FeedbackForm()
-        return render(request, 'feedback/feedback.html', context={
-            "form": form
-        })
+# # Let's make our logic with a class way not functional
+# # For working with filling and getting forms we have to declare get and/or post methods in our class
+# class FeedBackView(View):
+#     # Get the blank form
+#     def get(self, request):
+#         # Just create a blank form
+#         form = FeedbackForm()
+#         return render(request, 'feedback/feedback.html', context={
+#             "form": form
+#         })
+#
+#     # Filling the form and sending the results to the server
+#     def post(self, request):
+#         # The code below gets the blank forms and it's filled with the data from request
+#         form = FeedbackForm(request.POST)
+#         # So we are just checking whether the form is filled or not. If yes we make redirect, if not return the clean form
+#         if form.is_valid():
+#             # Getting the dictionary out of request parameters
+#             form.save()
+#             return HttpResponseRedirect("/done")
+#         return render(request, 'feedback/feedback.html', context={"form": form})
 
-    def post(self, request):
-        # The code below gets the blank forms and it's filled with the data from request
-        form = FeedbackForm(request.POST)
-        # So we are just checking whether the form is filled or not. If yes we make redirect, if not return the clean form
-        if form.is_valid():
-            # Getting the dictionary out of request parameters
-            form.save()
-            return HttpResponseRedirect("/done")
-        return render(request, 'feedback/feedback.html', context={"form": form})
+# It has the same logic as the class with the same name above
+class FeedBackView(FormView):
+    # We have to point what form(form class) we use
+    form_class = FeedbackForm
+    # We send the data to .html as 'form' variable. If we send something else we get nothing in response!!!
+    template_name = 'feedback/feedback.html'
+    # We have to point where we go after our submit has been approved
+    success_url = '/done'
+
+    # This method obliges us to do something with the data from the form
+    def form_valid(self, form):
+        form.save()
+        return super(FeedBackView, self).form_valid(form)
 
 
 class DoneView(View):
@@ -55,24 +71,6 @@ class FeedBackUpdateView(View):
             "form": form
         })
 
-# We can create a class with just a GET message is more easily with TemplateView
-# class ListFeedBack(TemplateView):
-#     template_name = "feedback/list_feedback.html"
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['all_feed'] = Feedback.objects.all()
-#         return context
-
-
-
-# class DetailFeedBack(TemplateView):
-#     template_name = "feedback/detail_feedback.html"
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context["one_row"] = Feedback.objects.get(id=kwargs["id_feedback"])
-#         return context
-
 
 # class 'ListView' is good for displaying all data from the DB
 class ListFeedBack(ListView):
@@ -93,7 +91,7 @@ class DetailFeedBack(DetailView):
     template_name = "feedback/detail_feedback.html"
     model = Feedback
     # To use DetailView we have to point the parameter with the special name 'pk'(primary key) in urls.py
-    # DetailView saves the needed data in 'context' variable with the name 'feedback'. So we have to indicate this name in .html
+    # DetailView saves the needed data in 'context' variable with the name of the model but in lowercase. So we have to indicate this name in .html
     # # But we can change that by setting the needed variable
     # context_object_name = "our_name"
 
