4045b8f05890bfcfc37eb9a9615b8e31cca282a8
AlexeyShakov <alexei_96@inbox.ru>
2022-08-04 19:34:32 +0300

    Implemented adding files through a form

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import GalleryUploadForm
+from django.http import HttpResponseRedirect
 
 # This func is needed to get the data(some file) and store it in another file
 # We use .chunks() in order to decrease the load from our RAM
@@ -13,8 +15,15 @@ def storage_file(file):
 # Create your views here.
 class GalleryView(View):
     def get(self, request):
-        return render(request, "gallery/load_file.html")
+        form = GalleryUploadForm()
+        # the parameter 'context' serves for sending the data to .html file
+        return render(request, "gallery/load_file.html", context={'form': form})
 
     def post(self, request):
-        storage_file(request.FILES['image'])
-        return render(request, "gallery/load_file.html")
+        # Filling the form with data
+        form = GalleryUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Saves a file
+            storage_file(request.FILES['image'])
+            return HttpResponseRedirect("load_image")
+        return render(request, "gallery/load_file.html", context={'form': form})
